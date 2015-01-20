2bab576007b8500fc31d6e137936173ed84564bc
sorong03 <sunwrobert@gmail.com>
2015-01-20 17:08:36 -0500

    Edits to NoteEdit to add in Color functionality

--- app/src/main/java/edu/gatech/androidnoteapp/NoteEditActivity.java
+++ app/src/main/java/edu/gatech/androidnoteapp/NoteEditActivity.java
@@ -1,9 +1,14 @@
 package edu.gatech.androidnoteapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * The activity that is used to edit notes.
@@ -11,11 +16,15 @@ import android.view.MenuItem;
  */
 public class NoteEditActivity extends ActionBarActivity {
 
+    EditText bodyText;
+    String[] colors = {"Red", "Green","Blue"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_edit);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Activates the actionbar button at the top
+
+        bodyText = (EditText) findViewById(R.id.note_text);
     }
 
     /**
@@ -30,20 +39,49 @@ public class NoteEditActivity extends ActionBarActivity {
 
     /**
      * Determines which item was selected
+     *
      * @param item the item which was selected
      * @return
      */
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home){ //Go back to the main page when the ActionBar button is tapped
+        int id = item.getItemId();
+        if (id == android.R.id.home) { //Go back to the main page when the ActionBar button is tapped
             saveAndFinish();
+        }
+        if(id == R.id.action_notecolor){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Pick a color");
+            builder.setTitle("Color");
+            AlertDialog dialog = builder.create();
         }
         return false;
     }
 
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick a color");
+        builder.setItems(colors, new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+                Toast.makeText(getApplicationContext(), "hi", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return builder.create();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_note_edit, menu);
+        return true;
+    }
     /**
      * Go back when the android back button is pressed
      */
     public void onBackPressed() {
         saveAndFinish();
+    }
+
+    public void populateView() {
+
     }
 }
